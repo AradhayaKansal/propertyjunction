50fe7a5e179b12f493f10c0088c07f46d7770ef4
Sourabh Singh Rathore <samy0009rathroe@gmail.com>
2016-04-04 01:18:38 +0530

    added Shard script

---
+++ src/PropertyJunction/wwwroot/Content/Scripts/Shared.js
@@ -0,0 +1,41 @@
+﻿var sliderTimer;
+
+$(document).ready(function () {
+    StartSlider();
+    $('.slider-nav').on('click', ChangeSlide);
+});
+
+function ChangeSlide() {
+    StopSlider();
+
+    if ($(this).hasClass('prev')) {
+        $('.slide-show > .slide.active')
+         .fadeOut(2000).removeClass('active')
+         .prev('.slide').fadeIn(2000).addClass('active')
+         .end()
+         .appendTo('.slide-show');
+    }
+    else {
+        $('.slide-show > .slide.active')
+        .fadeOut(2000).removeClass('active')
+        .next('.slide').fadeIn(2000).addClass('active')
+        .end()
+        .appendTo('.slide-show');
+    }
+
+    StartSlider();
+}
+
+function StartSlider() {
+    sliderTimer = setInterval(function () {
+        $('.slide-show > .slide.active')
+          .fadeOut(2000).removeClass('active')
+          .next('.slide').fadeIn(2000).addClass('active')
+          .end()
+          .appendTo('.slide-show');
+    }, 10000);
+}
+
+function StopSlider() {
+    clearInterval(sliderTimer);
+}
